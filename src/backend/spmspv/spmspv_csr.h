cd613e8a445a0c70d96412d081658c9fcd27ef3d
ludmik <slavakr01@mail.ru>
2021-11-29 22:57:51 +0300

    -parallel spmspv added

--- src/backend/spmspv/spmspv_csr.h
+++ src/backend/spmspv/spmspv_csr.h
@@ -8,13 +8,35 @@ struct bucket {
 #define INF 1000000000
 
 template <typename T>
+vector<vector<int>> estimate_buckets(MatrixCST<T> &matrix, SparseVector<T> &x, int nb, int nt)
+{
+    int nz = x.nz;
+    vector<vector<int>> Boffset(nt, vector<int>(nb));
+    #pragma omp parallel for schedule(static)
+    for (int t = 0; t < nt; t++) {
+        int offset = t * nz / nt;
+        for (int j = offset; j < offset + nt && j < nz; j++) {
+            int vector_index = x.ids[j];
+            for (int i = matrix.col_indx[vector_index]; i < matrix.col_indx[vector_index]; i++) {
+                int bucket_index = i * nb / matrix.size;
+                Boffset[t][bucket_index] += 1;
+            }
+        }
+    }
+}
+
+
+
+template <typename T>
 void SpMSpV(MatrixCSR<T> &matrix,
           SparseVector<T> &x,
           SparseVector<T> &y,
           number_of_buckets)
 {
+    vector<vector<int>> Boffset = estimate_buckets(matrix, x, number_of_buckets);
 
     // Step 1. Filling buckets.
+    #pragma omp parallel for schedule(static)
     vector<vector<bucket>> buckets(number_of_buckets);
     for (int i = 0; i < nz; i++) {
         int vector_index = x.ids[i];
@@ -28,6 +50,8 @@ void SpMSpV(MatrixCSR<T> &matrix,
 
     vector<int> SPA(matrix.size);
     vector<int> offset(number_of_buckets);
+
+    #pragma omp parallel for schedule(static)
     for (int k = 0; k < number_of_buckets; k++) {
         vector<int> uind;
         // Step 2. Merging entries in each bucket
