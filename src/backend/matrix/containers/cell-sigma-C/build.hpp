2d689cd2e0320fae23d7141b61280a9a5ac70279
Elijah <afanasiev_ilya@icloud.com>
2021-11-30 13:02:08 +0300

    - float/double spmv testing - a64fx vector groups in cell-sigma

--- src/backend/matrix/containers/cell-sigma-C/build.hpp
+++ src/backend/matrix/containers/cell-sigma-C/build.hpp
@@ -45,15 +45,19 @@ void MatrixCellSigmaC<T>::create_vertex_groups()
     vertex_groups[4].build(this, 16, 32);
     vertex_groups[5].build(this, 0, 16);
 
+    if(VECTOR_LENGTH == 32)
+        cell_c_start_group = 1;
+    else
+        throw "Error: incorrect VECTOR_LENGTH in MatrixCellSigmaC<T>::create_vertex_groups";
+
     cell_c_vertex_groups_num = 6;
-    cell_c_start_group = 3;
     cell_c_vertex_groups = new CSRVertexGroupCellC<T>[cell_c_vertex_groups_num];
-    cell_c_vertex_groups[0].build(this, 32, 64);
-    cell_c_vertex_groups[1].build(this, 16, 32);
-    cell_c_vertex_groups[2].build(this, 8, 16);
-    cell_c_vertex_groups[3].build(this, 4, 8);
-    cell_c_vertex_groups[4].build(this, 2, 4);
-    cell_c_vertex_groups[5].build(this, 0, 2);
+    cell_c_vertex_groups[0].build(this, 256, 512);
+    cell_c_vertex_groups[1].build(this, 128, 256);
+    cell_c_vertex_groups[2].build(this, 64, 128);
+    cell_c_vertex_groups[3].build(this, 32, 64);
+    cell_c_vertex_groups[4].build(this, 16, 32);
+    cell_c_vertex_groups[5].build(this, 0, 16);
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
