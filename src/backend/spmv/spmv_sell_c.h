6290caeafc5741fcbcc1838c870e72e5761a46d8
Elijah <afanasiev_ilya@icloud.com>
2021-12-01 15:24:41 +0300

    - correct spmv sellC

--- src/backend/spmv/spmv_sell_c.h
+++ src/backend/spmv/spmv_sell_c.h
@@ -13,35 +13,29 @@ void SpMV(const MatrixSellC<T> *_matrix,
     const T *x_vals = _x->get_vals();
     T *y_vals = _y->get_vals();
 
-    cout << "in sellC spmv" << endl;
-
-    /*#pragma omp parallel for schedule(static)
-    for(VNT row = 0; row< _matrix->size; ++row)
-    {
-        T tmp = 0;
-        for(VNT idx = _matrix->row_ptr[row]; idx < _matrix->row_ptr[row+1]; ++idx)
-        {
-            tmp += _matrix->vals[idx]*x_vals[_matrix->col_ids[idx]];
-        }
-        y_vals[row] = tmp;
-    }*/
-
     const int C = _matrix->C;
     const int  P = _matrix->P;
 
-    //#pragma omp parallel for schedule(static)
+    #pragma omp parallel for schedule(static)
     for(int chunk=0; chunk<_matrix->nchunks; ++chunk)
     {
-        for(int rowInChunk=0; rowInChunk<C; ++rowInChunk)
+        for(int rowInChunk=0; rowInChunk < C; ++rowInChunk)
         {
-            y_vals[chunk*C+rowInChunk] = 0;
+            if((chunk*C+rowInChunk) < _matrix->size)
+                y_vals[chunk*C+rowInChunk] = 0;
         }
+
         for(int j=0; j<_matrix->chunkLen[chunk]; j=j+P)
         {
             int idx = _matrix->chunkPtr[chunk]+j*C;
             for(int rowInChunk=0; rowInChunk<C; ++rowInChunk)
             {
-                y_vals[chunk*C+rowInChunk] += _matrix->valSellC[idx+rowInChunk]*x_vals[_matrix->colSellC[idx+rowInChunk]];
+                if((chunk*C+rowInChunk) < _matrix->size)
+                {
+                    T mat_val = _matrix->valSellC[idx+rowInChunk];
+                    VNT col_id = _matrix->colSellC[idx+rowInChunk];
+                    y_vals[chunk*C+rowInChunk] += mat_val * x_vals[col_id];
+                }
             }
         }
     }
