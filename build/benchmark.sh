b8711238e757ed1aee2126fe56360408c919722a
Elijah <afanasiev_ilya@icloud.com>
2022-01-07 15:24:22 +0300

    - xls stats saving

--- build/benchmark.sh
+++ build/benchmark.sh
@@ -1,9 +1,24 @@
-prefix="/home/afanasyev/LAGraph/src/benchmark/"
-graph_name="dbpedia.mtx"
-graph_path=$prefix"/"$graph_name
+function quit {
+  exit
+}
 
-rm perf_stats.txt
-./pr -graph mtx $graph_path
+function benchmark_graph {
+    prefix="/home/afanasyev/LAGraph/src/benchmark/"
+    app_name="./"$1
+    graph_name=$2
+    graph_path=$prefix"/"$graph_name
 
-python3 ./analize_data.py --graph=$graph_path
+    rm perf_stats.txt
+    $app_name -graph mtx $graph_path
+
+    python3 ./analize_data.py --graph=$graph_path
+}
+
+rm perf_dict.pkl
+
+benchmark_graph "pr" "flick.mtx"
+benchmark_graph "pr" "lj.mtx"
+
 python3 ./export_to_xls.py
+rm perf_dict.pkl
+rm perf_stats.txt
--- build/export_to_xls.py
+++ build/export_to_xls.py
@@ -3,6 +3,7 @@ import optparse
 import argparse
 import json
 import pickle
+import xlsxwriter
 
 
 def main():
@@ -10,6 +11,20 @@ def main():
     with open('perf_dict.pkl', 'rb') as f:
         full_perf_data = pickle.load(f)
     print(full_perf_data)
+
+    workbook = xlsxwriter.Workbook('perf_stats.xlsx')
+    worksheet_time = workbook.add_worksheet("time")
+
+    cnt_x = 0
+    for graph_name in full_perf_data.keys():
+        worksheet_time.write(0, cnt_x, graph_name)
+        cnt_y = 1
+        for func_name in full_perf_data[graph_name].keys():
+            worksheet_time.write(cnt_y, cnt_x, full_perf_data[graph_name][func_name]["time"])
+            cnt_y += 1
+        cnt_x += 1
+
+    workbook.close()
 
 
 if __name__ == "__main__":
