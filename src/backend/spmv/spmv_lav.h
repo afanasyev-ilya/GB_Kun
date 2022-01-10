4a2c3a236e1b27ea579ff6b63de3b5ea10b3cc5e
Elijah <afanasiev_ilya@icloud.com>
2022-01-10 12:53:55 +0300

    - loop prefetching

--- src/backend/spmv/spmv_lav.h
+++ src/backend/spmv/spmv_lav.h
@@ -39,13 +39,14 @@ void SpMV(const MatrixLAV<A> *_matrix,
         }
 
         VNT min_col = segment_data->min_col_id;
-        VNT max_col = segment_data->min_col_id;
+        VNT max_col = segment_data->max_col_id;
         #pragma omp for schedule(static)
-        for(VNT i = min_col; i < max_col; i++)
+        for(VNT i = min_col; i <= max_col; i++)
         {
             prefetched_vector[i - min_col] = x_vals[i];
         }
 
+        ENT proc_edges = 0;
         for(int vg = 0; vg < segment_data->vg_num; vg++)
         {
             const VNT *vertices = segment_data->vertex_groups[vg].get_data();
@@ -56,12 +57,13 @@ void SpMV(const MatrixLAV<A> *_matrix,
             {
                 VNT row = vertices[idx];
                 Y res = identity_val;
+
                 for(ENT j = segment_data->row_ptr[row]; j < segment_data->row_ptr[row + 1]; j++)
                 {
-                    VNT col = segment_data->col_ids[j];
+                    VNT col = segment_data->col_ids[j] - min_col;
                     Y mat_val = segment_data->vals[j];
-                    X x_val = prefetched_vector[col - min_col];
-                    res = add_op(res, mul_op(mat_val, x_vals[col]));
+                    X x_val = prefetched_vector[col];
+                    res = add_op(res, mul_op(mat_val, x_val));
                 }
                 shared_vector[row] = add_op(shared_vector[row], res);
             }
@@ -145,7 +147,6 @@ void SpMV(const MatrixLAV<A> *_matrix,
     }
     t2 = omp_get_wtime();
     cout << "sparse BW: " << _matrix->sparse_segment.nnz * (2*sizeof(A) + sizeof(VNT))/((t2 - t1)*1e9) << " GB/s" << endl;
-
 
     //reorder(y_vals, _matrix->new_to_old, _matrix_size);
     //reorder(y_vals, _matrix->new_to_old, _matrix_size);
