506d1347b86f35e01438458a6d71c15b775e0804
Elijah <afanasiev_ilya@icloud.com>
2022-01-19 13:16:22 +0300

    - seg csr inner timings

--- src/backend/matrix/containers/seg_csr/build.hpp
+++ src/backend/matrix/containers/seg_csr/build.hpp
@@ -28,6 +28,7 @@ void MatrixSegmentedCSR<T>::build(VNT _num_rows,
                                   const T *_vals,
                                   int _socket)
 {
+    double t1, t2;
     size = _num_rows;
     nnz = _nnz;
 
@@ -40,6 +41,7 @@ void MatrixSegmentedCSR<T>::build(VNT _num_rows,
     cout << "Seg size " << segment_size*sizeof(T)/1e3 << " KB" << endl;
 
     // create segments (must be reworked based on created CSR)
+    t1 = omp_get_wtime();
     subgraphs = new SubgraphSegment<T>[num_segments];
     for(VNT row = 0; row < _num_rows; row++)
     {
@@ -52,6 +54,8 @@ void MatrixSegmentedCSR<T>::build(VNT _num_rows,
             subgraphs[seg_id].add_edge(row, col, val);
         }
     }
+    t2 = omp_get_wtime();
+    cout << "subgraphs adding edges: " << t2 - t1 << " sec" << endl;
 
     merge_blocks_number = omp_get_max_threads()*2; // 2 for load balancing
     size_t merge_block_size = (size - 1) / merge_blocks_number + 1;
@@ -64,6 +68,7 @@ void MatrixSegmentedCSR<T>::build(VNT _num_rows,
     cout << "merge blocks count : " << merge_blocks_number << endl;
     cout << "merge_block_size : " << merge_block_size*sizeof(T) / 1024 << " KB" << endl;
 
+    t1 = omp_get_wtime();
     // construct CSRs and prepare merge blocks
     double avg_avg_degree = 0;
     for(int cur_seg = 0; cur_seg < num_segments; cur_seg++)
@@ -74,21 +79,18 @@ void MatrixSegmentedCSR<T>::build(VNT _num_rows,
         sorted_segments.push_back(make_pair(cur_seg, subgraphs[cur_seg].nnz));
         avg_avg_degree += ((double)subgraphs[cur_seg].nnz / subgraphs[cur_seg].size)/num_segments;
     }
+    t2 = omp_get_wtime();
+    cout << "converting subgraphs to CSR and merge blocks construction: " << t2 - t1 << " sec" << endl;
 
+    t1 = omp_get_wtime();
     // do load balancing optimization for the largest segment
     std::sort( std::begin(sorted_segments), std::end(sorted_segments), custome_compare );
-
 
     cout << "avg graph degree: " << ((double)nnz)/size << endl;
     cout << "avg avg degree: " << avg_avg_degree << endl;
     for(int cur_seg = 0; cur_seg < num_segments; cur_seg++)
     {
         int seg_id = sorted_segments[cur_seg].first;
-        cout << "seg " << seg_id<< " stats || ";
-        cout << "size (vertices) = " << subgraphs[seg_id].size << "(" <<
-             100.0*(double)subgraphs[seg_id].size/size << "%)" << ", nnz (edges) = " << subgraphs[seg_id].nnz << " (" <<
-             100.0*(double)subgraphs[seg_id].nnz/nnz << "%) ";
-        cout << "avg degree: " << (double)subgraphs[seg_id].nnz / subgraphs[seg_id].size << endl;
 
         double seg_avg_degree = (double)subgraphs[seg_id].nnz / subgraphs[seg_id].size;
         if(seg_avg_degree < 0.9*avg_avg_degree || percent_diff(seg_avg_degree, avg_avg_degree) < 0.1)
@@ -104,8 +106,15 @@ void MatrixSegmentedCSR<T>::build(VNT _num_rows,
         else
             subgraphs[seg_id].load_balanced_type = ONE_GROUP;
 
-        cout << "balancing: " << subgraphs[seg_id].schedule_type << " " << subgraphs[seg_id].load_balanced_type << endl;
+        /*cout << "seg " << seg_id<< " stats || ";
+        cout << "size (vertices) = " << subgraphs[seg_id].size << "(" <<
+             100.0*(double)subgraphs[seg_id].size/size << "%)" << ", nnz (edges) = " << subgraphs[seg_id].nnz << " (" <<
+             100.0*(double)subgraphs[seg_id].nnz/nnz << "%) ";
+        cout << "avg degree: " << (double)subgraphs[seg_id].nnz / subgraphs[seg_id].size << endl;
+        cout << "balancing: " << subgraphs[seg_id].schedule_type << " " << subgraphs[seg_id].load_balanced_type << endl;*/
     }
+    t2 = omp_get_wtime();
+    cout << "doing load balancing: " << t2 - t1 << " sec" << endl;
 }
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
