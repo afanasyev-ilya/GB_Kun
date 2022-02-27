28ae7fbd05850814fd970198dc3f38a23d3f8209
Elijah <afanasiev_ilya@icloud.com>
2022-02-27 09:34:44 +0300

    - prints enabled

--- algorithms/cc/cc.hpp
+++ algorithms/cc/cc.hpp
@@ -52,58 +52,56 @@ float cc(Vector<int>*       v,
     float gpu_tight_time = 0.f;
     int niter = 10;
 
-    for (iter = 1; iter <= niter; ++iter) {
+    for (iter = 1; iter <= niter; ++iter)
+    {
         // Duplicate parent.
         parent_temp.dup(&parent);
 
         // 1) Stochastic hooking.
         // mngf[u] = A x gf
         mxv(&min_neighbor_parent_temp, MASK_NULL, second<int>(),
-                                MinimumSelectSecondSemiring<int>(), A, &grandparent, desc);
+            MinimumSelectSecondSemiring<int>(), A, &grandparent, desc);
 
-        //cout << "min_neighbor_parent_temp: ";
-        //min_neighbor_parent_temp.print();
+        cout << "min_neighbor_parent_temp: ";
+        min_neighbor_parent_temp.print();
 
         eWiseAdd(&min_neighbor_parent, MASK_NULL, GrB_NULL,
-                                      minimum<int>(), &min_neighbor_parent,
-                                      &min_neighbor_parent_temp, desc);
+                 minimum<int>(), &min_neighbor_parent,
+                 &min_neighbor_parent_temp, desc);
 
-        //cout << "min_neighbor_paren: ";
-        //min_neighbor_parent.print();
+        cout << "min_neighbor_paren: ";
+        min_neighbor_parent.print();
 
         // f[f[u]] = mngf[u]. Second does nothing
         assignScatter(&parent, MASK_NULL, second<int>(),
-                                           &min_neighbor_parent, &parent_temp, parent_temp.nvals(), desc);
+                      &min_neighbor_parent, &parent_temp, parent_temp.nvals(), desc);
 
-        //cout << "after assign: ";
-        //parent.print();
+        cout << "after assign: ";
+        parent.print();
 
         // 2) Aggressive hooking.
         // f = min(f, mngf)
-        eWiseAdd(&parent, MASK_NULL, GrB_NULL,
-                 minimum<int>(), &parent, &min_neighbor_parent, desc);
+        eWiseAdd(&parent, MASK_NULL, GrB_NULL, minimum<int>(), &parent, &min_neighbor_parent, desc);
 
-        //cout << "after hooking: ";
-        //parent.print();
+        cout << "after hooking: ";
+        parent.print();
 
         // 3) Shortcutting.
         // f = min(f, gf)
-        eWiseAdd(&parent, MASK_NULL, GrB_NULL,
-                 minimum<int>(), &parent, &parent_temp, desc);
+        eWiseAdd(&parent, MASK_NULL, GrB_NULL, minimum<int>(), &parent, &parent_temp, desc);
 
         // 4) Calculate grandparents.
         // gf[u] = f[f[u]]
-        extract(&grandparent, MASK_NULL, second<int>(),
-                                           &parent, &parent, desc);
+        extract(&grandparent, MASK_NULL, second<int>(), &parent, &parent, desc);
 
         // 5) Check termination.
         cout << "grandparent: ";
         grandparent.print();
         cout << "grandparent_temp: ";
         grandparent_temp.print();
-        eWiseMult(&diff, MASK_NULL, GrB_NULL,
-                  lablas::not_equal_to<int>(), &grandparent_temp,
-                                        &grandparent, desc);
+
+        eWiseMult(&diff, MASK_NULL, GrB_NULL, lablas::not_equal_to<int>(), &grandparent_temp,
+                  &grandparent, desc);
         cout << "diff: ";
         diff.print();
         reduce<int, bool>(&succ, second<int>(), PlusMonoid<int>(), &diff, desc);
