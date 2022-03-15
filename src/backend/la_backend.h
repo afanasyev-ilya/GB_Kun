4b385619b33228fe6ce63ed7659133f2e4de2830
Elijah <afanasiev_ilya@icloud.com>
2022-03-15 11:49:14 +0300

    - spmv cleared from test

--- src/backend/la_backend.h
+++ src/backend/la_backend.h
@@ -30,7 +30,7 @@
 // CSR format settings
 #define CSR_SORTED_BALANCING 256
 #define __CSR_SEG_MERGE_SMALL__
-//#define __PARALLEL_TRANSPOSE__
+#define __PARALLEL_TRANSPOSE__
 //#define __CSR_PRINT_DATA_ARRAYS__
 
 #define __USE_SLICES__
