eb18350918bec9abf644c8522508ec37cb160619
flagro <antonpgrey@gmail.com>
2022-04-01 10:00:02 +0300

    added load balancing to the ijk algorithm

--- src/backend/spmspm/spmspm_ijk.h
+++ src/backend/spmspm/spmspm_ijk.h
@@ -30,39 +30,44 @@ void SpMSpM_ijk(const Matrix<T> *_matrix1,
     vector<T> values;
     ENT nnz = 0;
 
-    #pragma omp parallel for
-    for (VNT matrix1_row_id = 0; matrix1_row_id < matrix1_num_rows; ++matrix1_row_id) {
-        ENT matrix1_col_start_id = _matrix1->get_csr()->get_row_ptr()[matrix1_row_id];
-        ENT matrix1_col_end_id = _matrix1->get_csr()->get_row_ptr()[matrix1_row_id + 1];
-        ENT mask_col_start_id = _result_mask->get_csr()->get_row_ptr()[matrix1_row_id];
-        ENT mask_col_end_id = _result_mask->get_csr()->get_row_ptr()[matrix1_row_id + 1];
-        for (ENT mask_col_id = mask_col_start_id; mask_col_id < mask_col_end_id; ++mask_col_id) {
-            VNT matrix2_col_id = _result_mask->get_csr()->get_col_ids()[mask_col_id];
-            VNT matrix2_col_start_id = _matrix2->get_csc()->get_row_ptr()[matrix2_col_id];
-            VNT matrix2_col_end_id = _matrix2->get_csc()->get_row_ptr()[matrix2_col_id + 1];
+    auto offsets = _result_mask->get_csr()->get_load_balancing_offsets();
 
-            T accumulator = identity_val;
-            for (ENT matrix1_col_id = matrix1_col_start_id; matrix1_col_id < matrix1_col_end_id; ++matrix1_col_id) {
-                ENT matrix1_col_num = _matrix1->get_csr()->get_col_ids()[matrix1_col_id];
-                // i == matrix1_row_id, j == matrix2_col_id, k == matrix1_col_num
-                VNT found_matrix2_row_id = spgemm_binary_search(_matrix2->get_csc()->get_col_ids(),
-                                                                matrix2_col_start_id,
-                                                                matrix2_col_end_id - 1,
-                                                                matrix1_col_num);
+    #pragma omp parallel
+    {
+        const auto thread_id = omp_get_thread_num();
+        for (VNT matrix1_row_id = offsets[thread_id].first; matrix1_row_id < offsets[thread_id].second; ++matrix1_row_id) {
+            ENT matrix1_col_start_id = _matrix1->get_csr()->get_row_ptr()[matrix1_row_id];
+            ENT matrix1_col_end_id = _matrix1->get_csr()->get_row_ptr()[matrix1_row_id + 1];
+            ENT mask_col_start_id = _result_mask->get_csr()->get_row_ptr()[matrix1_row_id];
+            ENT mask_col_end_id = _result_mask->get_csr()->get_row_ptr()[matrix1_row_id + 1];
+            for (ENT mask_col_id = mask_col_start_id; mask_col_id < mask_col_end_id; ++mask_col_id) {
+                VNT matrix2_col_id = _result_mask->get_csr()->get_col_ids()[mask_col_id];
+                VNT matrix2_col_start_id = _matrix2->get_csc()->get_row_ptr()[matrix2_col_id];
+                VNT matrix2_col_end_id = _matrix2->get_csc()->get_row_ptr()[matrix2_col_id + 1];
 
-                if (found_matrix2_row_id != -1) {
-                    T matrix1_val = _matrix1->get_csr()->get_vals()[matrix1_col_id];
-                    T matrix2_val = _matrix2->get_csc()->get_vals()[found_matrix2_row_id];
-                    accumulator = add_op(accumulator, mul_op(matrix1_val, matrix2_val));
+                T accumulator = identity_val;
+                for (ENT matrix1_col_id = matrix1_col_start_id; matrix1_col_id < matrix1_col_end_id; ++matrix1_col_id) {
+                    ENT matrix1_col_num = _matrix1->get_csr()->get_col_ids()[matrix1_col_id];
+                    // i == matrix1_row_id, j == matrix2_col_id, k == matrix1_col_num
+                    VNT found_matrix2_row_id = spgemm_binary_search(_matrix2->get_csc()->get_col_ids(),
+                                                                    matrix2_col_start_id,
+                                                                    matrix2_col_end_id - 1,
+                                                                    matrix1_col_num);
+
+                    if (found_matrix2_row_id != -1) {
+                        T matrix1_val = _matrix1->get_csr()->get_vals()[matrix1_col_id];
+                        T matrix2_val = _matrix2->get_csc()->get_vals()[found_matrix2_row_id];
+                        accumulator = add_op(accumulator, mul_op(matrix1_val, matrix2_val));
+                    }
                 }
-            }
-            #pragma omp critical(updateresults)
-            {
-                if (accumulator) {
-                    row_ids.push_back(matrix1_row_id);
-                    col_ids.push_back(matrix2_col_id);
-                    values.push_back(accumulator);
-                    ++nnz;
+                #pragma omp critical(updateresults)
+                {
+                    if (accumulator) {
+                        row_ids.push_back(matrix1_row_id);
+                        col_ids.push_back(matrix2_col_id);
+                        values.push_back(accumulator);
+                        ++nnz;
+                    }
                 }
             }
         }
