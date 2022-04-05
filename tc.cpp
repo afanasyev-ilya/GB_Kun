430bd365442354f165fefcdb10cc293cb4ccc506
flagro <antonpgrey@gmail.com>
2022-04-05 08:16:15 +0300

    added directed and undirected triangle count for debug

--- tc.cpp
+++ tc.cpp
@@ -21,6 +21,7 @@ int main(int argc, char **argv) {
         matrix.get_nrows(&nrows);
 
         LAGraph_Graph<int> graph(matrix);
+        graph.A->get_matrix()->sort_csc_rows("STL_SORT");
         //graph.A->get_matrix()->print();
 
         //lablas::backend::Vector<int>* label_vector = new lablas::backend::Vector<int>(graph.A->get_matrix()->get_nrows());
@@ -49,10 +50,13 @@ int main(int argc, char **argv) {
             for (int i = 0; i < D.get_matrix()->get_nrows(); ++i) {
                 slow_ntriangles += D.get_matrix()->get_csr()->get(i, i);
             }
-            slow_ntriangles /= 6;
-            std::cout << "Found triangles with slow algorithm: " << slow_ntriangles << std::endl;
+            uint64_t slow_ntriangles_undir = slow_ntriangles / 6;
+            uint64_t slow_ntriangles_dir = slow_ntriangles / 3;
+            std::cout << "Found triangles on undirected graph with slow algorithm: " << slow_ntriangles_undir << std::endl;
+            std::cout << "Found triangles on directed graph with slow algorithm: " << slow_ntriangles_dir << std::endl;
             std::cout << "Error count: "
-                      << (slow_ntriangles > ntriangles ? slow_ntriangles - ntriangles : ntriangles - slow_ntriangles)
+                      << min((slow_ntriangles_undir > ntriangles ? slow_ntriangles_undir - ntriangles : ntriangles - slow_ntriangles_undir),
+                             (slow_ntriangles_dir > ntriangles ? slow_ntriangles_dir - ntriangles : ntriangles - slow_ntriangles_dir))
                       << std::endl;
         }
     }
