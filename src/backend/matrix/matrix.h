d88fd3d4e983420fb1adf3a1c54f82db3df51553
Elijah <afanasiev_ilya@icloud.com>
2022-04-15 05:52:57 +0300

    - testing graphs equality

--- src/backend/matrix/matrix.h
+++ src/backend/matrix/matrix.h
@@ -172,6 +172,10 @@ public:
     }
 
     void to_symmetric() { csr_data->to_symmetric(); csc_data->deep_copy(csr_data); };
+
+    friend bool operator==(const Matrix<T>& _lhs, const Matrix<T>& _rhs) {
+        return (_lhs.csr_data == _rhs.csr_data) && (_lhs.csc_data == _rhs.csc_data);
+    }
 private:
     MatrixContainer<T> *data;
     MatrixContainer<T> *transposed_data;
