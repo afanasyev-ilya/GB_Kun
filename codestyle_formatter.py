2e6e4f2f05ec4e9653f6f7ff606f14f378e3ca02
flagro <antonpgrey@gmail.com>
2022-06-05 03:28:49 +0300

    [CODESTYLE] Added support for pragmas and ifdef statements
    Now pragmas and ifdefs are processed by commenting lines that contain them and uncommenting them afterwards

--- codestyle_formatter.py
+++ codestyle_formatter.py
@@ -1,13 +1,11 @@
-import os
-import sys
-import argparse
-import subprocess
+import os, sys, argparse, subprocess, shutil, tempfile
 
 
 clang_format_file_path = "./.clang-format"
 allowed_file_extensions = [".c", ".cpp", ".h", ".hpp"]
 clang_format_executable = "clang-format"
 clang_format_arguments = "-style=file"
+lines_to_comment_out_prefixes = ["#pragma", "#ifdef", "#else", "#endif"]
 
 
 def get_files_to_format(directory_path):
@@ -22,14 +20,52 @@ def get_files_to_format(directory_path):
     return result
 
 
-def get_clang_format_diff(target_file_path):
-    result = subprocess.check_output([clang_format_executable,
-                                      clang_format_arguments, "--dry-run", target_file_path], stderr=STDOUT)
-    return result.replace("\\n", "\n")
+def preprocess_file(target_file_path):
+    with open(target_file_path, 'r') as file:
+        file_data = file.read()
+
+    for line_to_comment_out_prefix in lines_to_comment_out_prefixes:
+        file_data = file_data.replace(line_to_comment_out_prefix, "//" + line_to_comment_out_prefix)
+
+    with open(target_file_path, 'w') as file:
+        file.write(file_data)
 
 
-def apply_clang_format(target_file_path):
+def undo_preprocessing_file(target_file_path):
+    with open(target_file_path, 'r') as file:
+        file_data = file.read()
+
+    for line_to_comment_out_prefix in lines_to_comment_out_prefixes:
+        file_data = file_data.replace("//" + line_to_comment_out_prefix, line_to_comment_out_prefix)
+
+    with open(target_file_path, 'w') as file:
+        file.write(file_data)
+
+
+def get_clang_format_diff(target_file_path, safe_diff):
+    if not safe_diff:
+        tmp = tempfile.NamedTemporaryFile(suffix='.cpp', delete=False)
+        try:
+            shutil.copyfile(target_file_path, tmp.name)
+            preprocess_file(target_file_path)
+            result = subprocess.check_output([clang_format_executable, clang_format_arguments,
+                                              "--dry-run", tmp.name], stderr=subprocess.STDOUT)
+            undo_preprocessing_file(target_file_path)
+        finally:
+            tmp.close()
+            os.unlink(tmp.name)
+    else:
+        result = subprocess.check_output([clang_format_executable, clang_format_arguments,
+                                          "--dry-run", target_file_path], stderr=subprocess.STDOUT)
+    return str(result).replace("\\n", "\n")
+
+
+def apply_clang_format(target_file_path, safe_apply):
+    if not safe_apply:
+        preprocess_file(target_file_path)
     subprocess.run([clang_format_executable, clang_format_arguments, "-i", target_file_path])
+    if not safe_apply:
+        undo_preprocessing_file(target_file_path)
 
 
 def main():
@@ -38,6 +74,9 @@ def main():
                         action="store_true")
 
     parser.add_argument("-a", "--apply", help="Apply .clang-format CodeStyle to all project files",
+                        action="store_true")
+
+    parser.add_argument("-s", "--safe", help="Do check and apply without preprocessing",
                         action="store_true")
 
     args = parser.parse_args()
@@ -49,12 +88,12 @@ def main():
     files_to_format_paths = get_files_to_format(os.getcwd())
     for file_to_format_path in files_to_format_paths:
         if args.check:
-            check_result = get_clang_format_diff(file_to_format_path)
+            check_result = get_clang_format_diff(file_to_format_path, args.safe)
             if check_result:
                 print(f"Found problems with file: {file_to_format_path}:\n{check_result}0\n")
         if args.apply:
             print(f"Applied format to the file: {file_to_format_path}")
-            apply_clang_format(file_to_format_path)
+            apply_clang_format(file_to_format_path, args.safe)
 
 
 if __name__ == "__main__":
