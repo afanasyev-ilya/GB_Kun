eccf1825b51dcf3b30b567d86fdcc8cce94a8e10
Dmitry Lichmanov <dimlichmanov@gmail.com>
2022-06-18 17:08:38 +0300

    [ML_WRAPPER] More accurate scripting with parameters done
    Accurate script, no more dull if-checks, parameter checks done
    
    Signed-off-by: Dmitry Lichmanov <dimlichmanov@gmail.com>

--- ml_wrapper.py
+++ ml_wrapper.py
@@ -1,25 +1,36 @@
+from benchmark.scripts.helpers import *
+from benchmark.scripts.create_graphs_api import *
 import pickle
 import sys
 import os
 import subprocess
 import numpy as np
 
-
 if __name__ == "__main__":
+
+    # Set up label dictionary
+    label_dict = {0: "SEG_CSR", 1: "CSR"}
+
+    # Download trained model
 
     filename = 'model.sav'
     loaded_model = pickle.load(open(filename, 'rb'))
 
-    args = sys.argv[1:]
+    # Get script parameters
+
+    current_app = sys.argv[1]
+
+    args = sys.argv[2:]
     graphname = " ".join(args)
     print("Looking in the pickle for graph: ", graphname)
 
+    # Gather graph data from collected pickle file
+
     X_data = []
-    with open('all_graphs', 'rb') as f:
+    with open('dict.pickle', 'rb') as f:
         data_new = pickle.load(f)
         for k, v in data_new.items():
             if graphname == k:
-                print("OKOKOKO")
                 values = []
                 for item, value in v.items():
                     if item == "size":
@@ -28,23 +39,31 @@ if __name__ == "__main__":
                         values.append(value)
                     if item == "avg_degree":
                         values.append(value)
+                    if item == "percentile":
+                        values.append(value)
+                    if item == "exponent":
+                        values.append(value)
                 X_data.append(values)
 
     print(X_data)
 
+    # Predict label from downloaded model
+
     pred_label = loaded_model.predict(X_data)[0]
 
-    if pred_label == 0:
-        process = subprocess.Popen(['./bfs', '-format', 'CSR_SEG'],
-                       stdout=subprocess.PIPE,
-                       stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
+    # Running a application
 
-    if pred_label == 1:
-        process = subprocess.Popen(['./bfs', '-format', 'CSR'],
-                                   stdout=subprocess.PIPE,
-                                   stderr=subprocess.PIPE)
-        stdout, stderr = process.communicate()
+    mult = 10
+    file_extension = "mtx"
+
+    common_args = ["-it", str(common_iterations * mult), "-format", label_dict[pred_label], "-no-check"]
+
+    cmd = ["bash", "./benchmark.sh", get_binary_path(current_app), "-graph mtx ",
+           get_path_to_graph(graphname, file_extension)] + common_args
+    print("! ! ! " + get_path_to_graph(graphname, file_extension))
+    print(' '.join(cmd))
+    proc = subprocess.Popen(cmd, shell=False, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    proc.wait()
+    stdout, stderr = proc.communicate()
 
     print(stdout)
-
