d6551c8c0e2b1eb69bcff99d225169c50f181f9e
flagro <antonpgrey@gmail.com>
2022-07-09 12:22:44 +0300

    [GRAPH MODIFICATIONS] Added draft of modification push
    Added draft of modification push

--- src/backend/matrix/containers/csr/modification.hpp
+++ src/backend/matrix/containers/csr/modification.hpp
@@ -2,6 +2,35 @@
 
 /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
+namespace {
+
+template<typename T>
+bool added_edge_is_valid(VNT asking_row, const std::pair<VNT, ENT>& edge_pair, T edge_weight,
+                         const std::map<VNT, std::map<std::pair<VNT, ENT>, T> >& added_edges) {
+    const auto &edge_src_id = edge_pair.first;
+    const auto &edge_dst_id = edge_pair.second;
+    bool edge_is_valid = true;
+    if (asking_row != edge_src_id) {
+        if (added_edges.find(edge_src_id) == added_edges.end() or
+            added_edges[edge_src_id].find(edge_pair) == added_edges[edge_src_id].end() or
+            added_edges[edge_src_id][edge_pair] != edge_weight) {
+            edge_is_valid = false;
+        }
+    }
+    if (asking_row != edge_dst_id) {
+        if (added_edges.find(edge_dst_id) == added_edges.end() or
+            added_edges[edge_dst_id].find(edge_pair) == added_edges[edge_dst_id].end() or
+            added_edges[edge_dst_id][edge_pair] != edge_weight) {
+            edge_is_valid = false;
+        }
+    }
+    return edge_is_valid;
+}
+
+}
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 template<typename T>
 void MatrixCSR<T>::add_row(VNT _row)
 {
@@ -77,69 +106,67 @@ void MatrixCSR<T>::remove_val(VNT _row, VNT _col)
 template<typename T>
 void MatrixCSR<T>::apply_modifications()
 {
-    if(num_changes < 1000) // TODO improve this criteria
-        return;
-
-    std::unordered_map<VNT, std::unordered_map<VNT, T>> new_graph;
-
     VNT new_nrows = nrows;
     VNT new_ncols = 0;
-    ENT new_nnz = 0;
-    for(const auto &[row, row_data]: new_matrix_rows)
-        new_nrows = std::max(row, new_nrows);
+    std::vector<VNT> new_row_ptr;
+    new_row_ptr.push_back(0);
+    std::vector<ENT> new_col_ids;
+    std::vector<T> new_vals;
 
-    // remove rows marked for deletion and save others into temporary graph
-    for(VNT row = 0; row < nrows; row++)
-    {
-        if(!row_marked_for_removal(row))
-        {
-            for(ENT i = row_ptr[row]; i < row_ptr[row + 1]; i++)
-            {
-                VNT col = col_ids[i];
-                VNT val = vals[i];
-                if(!val_marked_for_removal(i)) {
-                    new_graph[row][col] = val;
-                    new_ncols = std::max(col, new_ncols);
-                    new_nnz++;
-                }
-            }
-        }
+    for (const auto row: added_rows) {
+        new_nrows = std::max(row, new_nrows);
     }
 
-    // add new vertices
-    for(const auto &[row, row_data]: new_matrix_rows)
-    {
-        for(const auto &[col, val]: row_data)
-        {
-            new_graph[row][col] = val;
-            new_ncols = std::max(col, new_ncols);
-            new_nnz++;
+    for (VNT row = 0; row < new_nrows; ++row) {
+        VNT cur_row_nnz = 0;
+        if (row < nrows) {
+            // add old vertices
+            if (removed_rows.find(row) != removed_rows.end()) {
+                if (restored_rows.find(row) != restored_rows.end()) {
+                    for (const auto &[edge_pair, edge_weight] : added_edges[row]) {
+                        if (edge_pair.first == row and added_edge_is_valid(row, edge_pair, edge_weight, added_edges)) {
+                            new_col_ids.push_back(edge_pair.second);
+                            new_ncols = std::max(new_ncols, edge_pair.second);
+                            new_col_ids.push_back(edge_weight);
+                            ++cur_row_nnz;
+                        }
+                    }
+                }
+            } else {
+                // TODO: implement this case
+            }
+        } else {
+            // add new vertices
+            // TODO: implement this case
         }
+        new_row_ptr.push_back(new_row_ptr.back() + cur_row_nnz);
     }
 
     // clear datastructures, which store updates
-    new_matrix_rows.clear();
     removed_rows.clear();
+    restored_rows.clear();
+    added_rows.clear();
     removed_edges.clear();
+    added_edges.clear();
 
     nrows = new_nrows;
     ncols = new_ncols;
-    nnz = new_nnz;
-
+    nnz = new_col_ids.size();
     resize(nrows, ncols, nnz);
-
-    ENT cur_pos = 0;
-    for(const auto &[row, row_data]: new_matrix_rows)
-    {
-        row_ptr[row] = cur_pos;
-        row_ptr[row + 1] = cur_pos + row_data.size();
-        for(const auto &[col, val]: row_data)
-        {
-            col_ids[cur_pos] = col;
-            vals[cur_pos] = val;
-            cur_pos++;
-        }
+    #pragma omp parallel
+    for (VNT i = 0; i < nrows; ++i) {
+        row_ptr[i] = new_row_ptr[i];
     }
+    #pragma omp parallel
+    for (VNT i = 0; i < nnz; ++i) {
+        col_ids[i] = new_col_ids[i];
+    }
+    #pragma omp parallel
+    for (VNT i = 0; i < nnz; ++i) {
+        vals[i] = new_vals[i];
+    }
+
+    calculate_degrees();
 
     ongoing_modifications = false;
     num_changes = 0;
