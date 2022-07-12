32ab833608dc8428bb007b5f074fe16ad25deb7d
flagro <antonpgrey@gmail.com>
2022-07-12 13:09:09 +0300

    [GRAPH MODIFICATIONS] Implemented base of apply modification function for testing
    Implemented base of apply modification function for testing

--- graph_modification.cpp
+++ graph_modification.cpp
@@ -1,7 +1,46 @@
 #include "src/gb_kun.h"
 
-enum class ModificationTypes { VERTEX_INSERTION_OP = 1, VERTEX_DELETION_OP = 2, EDGE_INSERTION_OP = 3, EDGE_DELETION_OP = 4 };
+enum ModificationTypes { VERTEX_INSERTION_OP, VERTEX_DELETION_OP, EDGE_INSERTION_OP, EDGE_DELETION_OP, MOD_TYPES_LEN };
 
+VNT get_random_vertex_id_to_insert(VNT nrows, VNT ncols) {
+    return (rand() % ((nrows * 3)));
+}
+
+VNT get_random_vertex_id_to_delete(VNT nrows, VNT ncols) {
+    return (rand() % ((int)((double) nrows * 1.2)));
+}
+
+std::pair<VNT, VNT> get_random_edge_pair(VNT nrows, VNT ncols) {
+    return std::make_pair(rand() % ((int)((double) nrows * 1.2)), rand() % ((int)((double) ncols * 1.2)));
+}
+
+float get_random_edge_weight_value() {
+    return rand() % 5 + 1;
+}
+
+ModificationTypes get_random_modification() {
+    const int mod_types_len_value = MOD_TYPES_LEN;
+    return static_cast<ModificationTypes>(rand() % mod_types_len_value);
+}
+
+void apply_modification(ModificationTypes modification_type, lablas::Matrix<float> &A, std::vector<std::vector<float> > &A_clone, VNT &nrows, VNT &ncols) {
+    if (modification_type == VERTEX_INSERTION_OP) {
+        const auto vertex_id = get_random_vertex_id_to_insert(nrows, ncols);
+        cout << "Applying vertex insertion of <" << vertex_id << ">" << endl;
+    } else if (modification_type == VERTEX_DELETION_OP) {
+        const auto vertex_id = get_random_vertex_id_to_delete(nrows, ncols);
+        cout << "Applying vertex deletion of <" << vertex_id << ">" << endl;
+    } else if (modification_type == EDGE_INSERTION_OP) {
+        const auto [src_id, dst_id] = get_random_edge_pair(nrows, ncols);
+        const auto edge_weight = get_random_edge_weight_value();
+        cout << "Applying edge insertion of (" << src_id << ", " << dst_id << ", " << edge_weight << ")" << endl;
+    } else if (modification_type == EDGE_DELETION_OP) {
+        const auto [src_id, dst_id] = get_random_edge_pair(nrows, ncols);
+        cout << "Applying edge deletion of (" << src_id << ", " << dst_id <<  ")" << endl;
+    } else {
+        throw "unknown modification type";
+    }
+}
 
 int main(int argc, char **argv) {
     try
@@ -16,8 +55,8 @@ int main(int argc, char **argv) {
         init_matrix(A, parser);
 
         if (parser.check()) {
-            const auto cur_nrows = A.nrows();
-            const auto cur_ncols = A.ncols();
+            auto cur_nrows = A.nrows();
+            auto cur_ncols = A.ncols();
 
             std::vector<std::vector<float> > A_clone(cur_nrows, std::vector<float>(cur_ncols));
             int error_cnt = 0;
@@ -31,7 +70,7 @@ int main(int argc, char **argv) {
             const int op_cnt = 10;
             for (int cur_block_op = 0; cur_block_op < op_blocks_cnt; ++cur_block_op) {
                 for (int op_id = 0; op_id < op_cnt; ++op_id) {
-                    // generate modifications and update matrices here
+                    apply_modification(get_random_modification(), A, A_clone, cur_nrows, cur_ncols);
                 }
 
                 A.get_matrix()->apply_modifications();
@@ -46,9 +85,7 @@ int main(int argc, char **argv) {
 
             }
 
-
-            cout << "error_count: " << error_cnt << "/"
-                 << A.get_matrix()->get_nrows() * A.get_matrix()->get_nrows() << endl;
+            cout << "error_count: " << error_cnt << "/" << cur_nrows * cur_ncols << endl;
         }
     }
     catch (string& error)
