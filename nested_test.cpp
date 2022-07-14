9816dcfeb8c93bb789b46aa9720d6185128e3fcf
Dmitry Lichmanov <dimlichmanov@gmail.com>
2022-07-15 01:18:47 +0300

    [NESTED] Nested script done, measurements are ready
    Nested script reformatted
    
    Signed-off-by: Dmitry Lichmanov <dimlichmanov@gmail.com>

--- nested_test.cpp
+++ nested_test.cpp
@@ -1,57 +1,15 @@
 #include "src/gb_kun.h"
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void save_to_file(const string &_file_name, double _stat)
-{
-    ofstream stat_file;
-    stat_file.open(_file_name, std::ios_base::app);
-    stat_file << _stat << endl;
-    stat_file.close();
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-void report_num_threads(int level)
-{
-#pragma omp single
-    {
-        printf("Level %d: number of threads in the team - %d\n",
-               level, omp_get_num_threads());
-    }
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
-
-template <typename T>
-void check_mxv(lablas::Vector<T> &_out, lablas::Matrix<T> &_matrix, lablas::Vector<T> &_in)
-{
-    T* out_vals = _out.get_vector()->getDense()->get_vals();
-    T* in_vals = _in.get_vector()->getDense()->get_vals();
-    lablas::backend::MatrixCSR<T> *csr_matrix = ((lablas::backend::MatrixCSR<T> *) _matrix.get_matrix()->get_csr());
-
-    Index num_rows;
-    _matrix.get_nrows(&num_rows);
-    for(VNT row = 0; row < num_rows; row++)
-    {
-        T res = 0;
-        for(ENT j = csr_matrix->get_row_ptr()[row]; j < csr_matrix->get_row_ptr()[row + 1]; j++)
-        {
-            VNT col = csr_matrix->get_col_ids()[j];
-            T val = csr_matrix->get_vals()[j];
-            res = res + val * in_vals[col];
-        }
-        out_vals[row] = res;
-    }
-}
-
-/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+#include <cstdlib>
+#define nested_param 10
 
 template<typename T>
-void test_spmv(int argc, char **argv)
+void test_nested_spmv(int argc, char **argv)
 {
+    setenv("OMP_NESTED", "FALSE", 1);
     Parser parser;
     parser.parse_args(argc, argv);
+
+    double sequential_runs[nested_param];
 
     lablas::Descriptor desc;
 
@@ -59,11 +17,58 @@ void test_spmv(int argc, char **argv)
 
 
     int num_runs = parser.get_iterations();
+
+    double repeating_start = omp_get_wtime();
+
+    for (int iteration = 0; iteration < nested_param; iteration++) {
+
+        lablas::Matrix<T> matrix;
+        matrix.set_preferred_matrix_format(parser.get_storage_format());
+        init_matrix(matrix, parser);
+
+        GrB_Index size;
+        matrix.get_nrows(&size);
+        lablas::Vector<T> w(size);
+        lablas::Vector<T> u(size);
+
+
+        Index sparsity_k = 1000.0;
+        vector <GrB_Index> nnz_subset;
+        for (Index i = 0; i < size / sparsity_k + 1; i++)
+            nnz_subset.push_back(rand() % size);
+
+        Index u_const = 1;
+        Index u_diff_const = 5;
+        u.fill(u_const);
+        GrB_TRY(GrB_assign(&u, MASK_NULL, NULL, u_diff_const, &(nnz_subset[0]), nnz_subset.size(), NULL));
+        /* We don't need to do a heating run */
+
+        double avg_time = 0;
+
+        for(int run = 0; run < num_runs; run++)
+        {
+            w.fill(1.0);
+
+            double t1 = omp_get_wtime();
+            SAVE_STATS(GrB_mxv(&w, MASK_NULL, NULL, lablas::PlusMultipliesSemiring<T>(), &matrix, &u, &desc);,
+                       "SPMV", (sizeof(float)*2 + sizeof(size_t)), 1, &matrix);
+            double t2 = omp_get_wtime();
+            avg_time += (t2 - t1) / num_runs;
+        }
+    }
+
+    double repeating_end = omp_get_wtime();
+
+    std::cout << "Overall sequential run time is " << repeating_end - repeating_start << std::endl;
+    std::cout << "Average sequential run time is " << (repeating_end - repeating_start) / nested_param << std::endl;
+
+    setenv("OMP_NESTED", "TRUE", 1);
+    printf("OMP_NESTED = %s\n", getenv("OMP_NESTED"));
     double nested_start = omp_get_wtime();
 
 /* Following region is being run by N threads, each of the threads is having own matrix and vector "copy" */
-#pragma omp parallel num_threads(10)
-    {
+#pragma omp parallel num_threads(nested_param)
+{
 
     lablas::Matrix<T> matrix;
     matrix.set_preferred_matrix_format(parser.get_storage_format());
@@ -101,56 +106,26 @@ void test_spmv(int argc, char **argv)
 
 #pragma omp critical
     {
-        std::cout << "Average mxv time for thread " << omp_get_thread_num() << " is " << avg_time << " (num_runs = "<< num_runs << ")" << std::endl;
+        sequential_runs[omp_get_thread_num()] = avg_time;
     }
-    }
-
+}
     double nested_end = omp_get_wtime();
-    std::cout << "Overall nested time is " << nested_end - nested_start << std::endl;
 
-    /*
-    int num_runs = parser.get_iterations();
-    double avg_time = 0;
-    for(int run = 0; run < num_runs; run++)
-    {
-        w.fill(1.0);
-
-        double t1 = omp_get_wtime();
-        SAVE_STATS(GrB_mxv(&w, MASK_NULL, NULL, lablas::PlusMultipliesSemiring<T>(), &matrix, &u, &desc);,
-                   "SPMV", (sizeof(float)*2 + sizeof(size_t)), 1, &matrix);
-        double t2 = omp_get_wtime();
-        avg_time += (t2 - t1) / num_runs;
+    double max_time = 0.0, min_time = INT32_MAX, avg_time = 0.0;
+    for (int iter = 0; iter < nested_param; iter++) {
+        avg_time += sequential_runs[iter];
+        if (sequential_runs[iter] > max_time) {
+            max_time = sequential_runs[iter];
+        }
+        if (sequential_runs[iter] < min_time) {
+            min_time = sequential_runs[iter];
+        }
     }
+    avg_time /= nested_param;
 
-    double perf = 2.0*matrix.get_nnz()/(avg_time*1e9);
-    double bw = (2.0*sizeof(T)+sizeof(Index))*matrix.get_nnz()/(avg_time*1e9);
-    cout << "SPMV avg time: " << avg_time*1000 << " ms" << endl;
-    cout << "SPMV avg perf: " << perf << " GFlop/s" << endl;
-    cout << "SPMV avg BW: " << bw << " GB/s" << endl;
-    save_to_file("./output/perf.txt", perf);
-    save_to_file("./output/bw.txt", bw);
-
-    if(parser.check()) // can check only for external graph for now
-    {
-        lablas::Vector<T> w_check(size);
-
-        u.fill(u_const);
-        w_check.fill(1.0);
-        GrB_TRY(GrB_assign(&u, MASK_NULL, NULL, u_diff_const, &(nnz_subset[0]), nnz_subset.size(), NULL));
-
-        check_mxv(w_check, matrix, u);
-
-        if(w == w_check)
-        {
-            print_diff(w, w_check);
-            cout << "Vectors are equal" << endl;
-        }
-        else
-        {
-            print_diff(w, w_check);
-            cout << "Vectors are NOT equal" << endl;
-        }
-    }*/
+    std::cout << "Overall nested time is " << nested_end - nested_start << std::endl;
+    std::cout << "Deviation+ = " << max_time-avg_time <<", Deviation- = " << avg_time-min_time << std::endl;
+    std::cout << "Average thread running time = " << avg_time << std::endl;
 
 #undef MASK_NULL
 }
@@ -161,8 +136,7 @@ int main(int argc, char **argv)
 {
     try
     {
-        print_omp_stats();
-        test_spmv<float>(argc, argv);
+        test_nested_spmv<float>(argc, argv);
     }
     catch (string error)
     {
