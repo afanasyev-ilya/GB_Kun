eea9b9b7b57cb2fdf31e5d77d14fb4b91c7250e7
Sviatoslav <slavakr01@mail.ru>
2022-07-20 11:50:04 +0300

    [gtest] mxv test (Does not work yet).

---
+++ unit_tests.cpp
@@ -0,0 +1,58 @@
+#ifdef __USE_GTEST__
+#include <gtest/gtest.h>
+#endif
+
+#include "src/gb_kun.h"
+
+#define SIZE 5
+#define MASK_NULL static_cast<const lablas::Vector<float>*>(NULL)
+
+class TestFoo : public ::testing::Test
+{
+};
+
+TEST_F(TestFoo, test1)
+{
+    lablas::Vector<int> u(SIZE);
+    lablas::Vector<int> w(SIZE);
+    lablas::Vector<int> ans(SIZE);
+
+    int* ans_vals = ans.get_vector()->getDense()->get_vals();
+    ans_vals[0] = 9;
+    ans_vals[1] = 14;
+    ans_vals[2] = 12;
+    ans_vals[3] = 6;
+    ans_vals[4] = 1;
+
+    int* u_vals = u.get_vector()->getDense()->get_vals();
+    for (int i = 0; i < 5; i++) {
+        u_vals[i] = i + 1;
+    }
+
+    lablas::Matrix<int> A;
+    A.init_from_mtx("test_matrix.mtx");
+    A.print();
+
+    lablas::Descriptor desc;
+    mxv(&w, MASK_NULL, GrB_NULL, lablas::PlusMultipliesSemiring<int>(), &A, &u, &desc);
+
+    ASSERT_EQ(w, ans);
+}
+
+int main(int argc, char **argv) {
+    try
+    {
+        ::testing::InitGoogleTest(&argc, argv);
+        return RUN_ALL_TESTS();
+    }
+    catch (string& error)
+    {
+        cout << error << endl;
+    }
+    catch (const char * error)
+    {
+        cout << error << endl;
+    }
+    return 0;
+}
+
