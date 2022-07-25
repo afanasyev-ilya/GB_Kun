ee80c1f03398b870b1443219188ae5259b3d3b1e
flagro <antonpgrey@gmail.com>
2022-07-25 17:09:02 +0200

    [BENCHMARK] Replaced friendster graph with epinions graph
    Replaced friendster graph with epinions graph for twenty graphs set for testing

--- benchmark/scripts/settings.py
+++ benchmark/scripts/settings.py
@@ -389,4 +389,4 @@ konect_best_fast = ['soc_libimseti', 'soc_pokec', 'web_dimacs10-cnr-2000', 'web_
 konect_twenty_set = ['soc_libimseti', 'soc_pokec', 'web_dimacs10-cnr-2000', 'web_eu_2005', 'soc_livemocha',
                      'soc_livejournal', 'web_zhishi', 'web_wikipedia_links_ru', 'web_wikipedia_links_it', 'web_hudong',
                      'web_wikipedia_links_sv', 'soc_orkut', 'web_web_trackers', 'web_dbpedia_links', 'web_uk_domain_2002',
-                     'soc_twitter_www', 'soc_friendster', 'soc_catster_dogster', 'road_texas', 'road_colorado']
+                     'soc_twitter_www', 'rating_epinions', 'soc_catster_dogster', 'road_texas', 'road_colorado']
